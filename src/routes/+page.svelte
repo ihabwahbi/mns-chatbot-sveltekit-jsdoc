08272469363a1869f362ce1f3839c01c29bca139
Ihab Wahbi <ihab.a.wahbi@gmail.com>
2024-03-09 01:47:15 +1030

    Change loading animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -167,17 +167,23 @@
 					</div>
 				{/each}
 				{#if $isLoading}
-					<div><strong class="text-slbBlue">EchoEngine:</strong></div>
+					<div class="flex items-center">
+						<div class="flex-none pr-2"><strong class=" text-slbBlue">EchoEngine: </strong></div>
+						<div
+							class="delay-400 h-2 flex-grow animate-pulse flex-row space-x-4 rounded bg-slate-400"
+						></div>
+					</div>
 					<div class="flex animate-pulse space-x-4">
-						<div class="flex-1 space-y-6 py-1">
-							<div class="h-2 rounded bg-slate-400"></div>
-							<div class="space-y-3">
-								<div class="grid grid-cols-3 gap-4">
-									<div class="col-span-2 h-2 rounded bg-slate-400"></div>
-									<div class="col-span-1 h-2 rounded bg-slate-400"></div>
-								</div>
-								<div class="h-2 rounded bg-slate-400"></div>
+						<div class="flex-1 space-y-3 py-1">
+							<div class="grid grid-cols-3 gap-1">
+								<div class="col-span-1 h-2 rounded bg-slate-400"></div>
+								<div class="col-span-2 h-2 rounded bg-slate-400"></div>
 							</div>
+							<div class="grid grid-cols-3 gap-1">
+								<div class="col-span-2 h-2 rounded bg-slate-400"></div>
+								<div class="col-span-1 h-2 rounded bg-slate-400"></div>
+							</div>
+							<div class="h-2 rounded bg-slate-400"></div>
 						</div>
 					</div>
 				{/if}
